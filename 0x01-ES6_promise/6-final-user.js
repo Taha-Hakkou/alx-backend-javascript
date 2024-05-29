0fd673c83caf7c03a1a6e701760dd4005fd87549
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-05-29 22:07:08 +0100

    0x01 6-final-user corr

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.resolve([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results);
 }
