0e2e955f1ed8114fe5d28b42c950c1bff2854ffb
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-05-31 00:27:28 +0100

    0x02 4-pricing corr

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,6 +3,9 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be an instance of Currency');
+    }
     this._currency = currency;
   }
 
@@ -12,12 +15,21 @@ export default class Pricing {
 
   set amount(value) { this._amount = value; }
 
-  set currency(value) { this._currency = value; }
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
 
   displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
+  /**
+   * @param {Number} amount
+   * @param {Number} conversionRate
+   */
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
