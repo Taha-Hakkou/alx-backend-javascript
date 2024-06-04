fa2f87c150bdde713feb88192201c5fff34a6aeb
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-06-04 19:08:53 +0100

    0x03 8-clean_set corr

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
   return Array.from(set)
-    .filter((x) => x.startsWith(startString) && startString.length > 0)
+    .filter(
+      (x) => typeof x === 'string'
+          && x.startsWith(startString)
+          && startString.length > 0,
+    )
     .map((x) => x.substring(startString.length))
     .join('-');
 }
