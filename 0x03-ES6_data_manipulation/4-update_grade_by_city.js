53526d06178dadcd386172b92c1fd4b082efbe27
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-06-04 18:28:45 +0100

    0x03 4-update_grade_by_city corr

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,12 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-  const gradedStudents = listStudents.filter((student) => student.location === city);
+  const gradedStudents = listStudents.filter(
+    (student) => student.location === city,
+  );
   gradedStudents.forEach((student) => {
-    const newGrade = newGrades.find((x) => x.studentId === student.id);
+    const newGrade = newGrades.find(
+      (x) => x.studentId === student.id,
+    );
+    // eslint-disable-next-line no-param-reassign
     student.grade = newGrade ? newGrade.grade : 'N/A';
   });
   return gradedStudents;
