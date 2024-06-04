7d8d0366190517fc3e0e7ea188fb8df3d9053645
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-06-04 10:40:30 +0100

    0x03 5-typed_array & 7-has_array_values corr

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const typedArray = new DataView(new ArrayBuffer(length));
-  if (position < 0 || position >= typedArray.length) {
-    throw Error('Position outside range');
-  }
+  if (position > typedArray.length) throw Error('Position outside range');
   typedArray.setInt8(position, value);
   return typedArray;
 }
